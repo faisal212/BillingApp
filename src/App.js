86519d14cef6450736dff226d3072e6b47777ca7
Faisal Aqdas <faisalaqdas@gmail.com>
2019-10-21 23:27:21 +0500

    update base

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React,{useEffect} from 'react';
 import logo from './logo.svg';
+import base64 from 'base-64';
 import './App.css';
-
 function App() {
+  const user = "ssp-root";
+  const password = "Lahore@007";
+
   const getXml = () => {
     fetch('https://portal.onairtel.com/xmlapi/xmlapi', {
       method: 'POST',
-      headers: {'Content-Type': 'text/xml'},
+      headers: new Headers( { 'Authorization': 'Basic ' + base64.encode(user + ":" + password)} ) ,
       body: '<?xml version="1.0"?> <methodCall> <methodName>listCustomers</methodName><params><param><value><struct> <member> </member> </struct></value></param> </params> </methodCall>' 
     })
     .then(response => console.log(response))
@@ -17,7 +20,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reloads.
         </p>
         <button onClick={getXml}
         >Get xml</button>
